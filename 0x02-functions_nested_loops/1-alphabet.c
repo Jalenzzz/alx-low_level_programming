b9116c88385c17fff5c51b2cf1ea17921bf66368
Jalenzzz <mnenejalen72@gmail.com>
2023-03-22 14:59:11 -0700

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,22 @@
 #include "main.h"
 
 /**
- * print_alphabet - Make the alphabet
- * Return_void
-*/
-
+ * print_alphabet - Prints the alphabet in lowercase
+ *
+ * Description: Uses _putchar function to print each letter of the alphabet
+ *              in lowercase followed by a newline character
+ *
+ * Return: void
+ */
 void print_alphabet(void)
-
 {
-
     char c;
 
     for (c = 'a'; c <= 'z'; c++)
     {
         _putchar(c);
     }
+
     _putchar('\n');
 }
+
